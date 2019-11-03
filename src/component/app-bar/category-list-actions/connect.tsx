3b6ca694001db859f31ca02e0cdaa8a789494e65
Omer Prizner <omer.prizner@gmail.com>
2019-11-03 13:09:58 +0200

    display category list actions only when in categories screen.

--- src/component/app-bar/category-list-actions/connect.tsx
+++ src/component/app-bar/category-list-actions/connect.tsx
@@ -2,37 +2,46 @@ import React from "react";
 import { useObserver } from "mobx-react";
 import { CategoryListActions } from ".";
 import { useStore } from "../../common/provider/store";
-import { useHistory } from "react-router";
+import { useHistory, useLocation } from "react-router";
 
 export const ConnectCategoryListActions: React.FC = () => {
   const store = useStore();
   const history = useHistory();
+  const location = useLocation();
 
   return useObserver(
-    () => (
-      <CategoryListActions
-        selectedCategoryId={store.categoriesManager.selectedId}
-        onCreateCategory={() => {
-          console.log('Routing to add category');
-          history.push('/categories/create');
-        }}
-        onEditCategory={() => {
-          console.log('Routing to edit category');
-          history.push(`/categories/edit/${store.categoriesManager.selectedId}`);
-        }}
-        onViewCategory={() => {
-          console.log('Also routing to edit category');
-          history.push(`/categories/edit/${store.categoriesManager.selectedId}`);
-        }}
-        onDeleteCategory={() => {
-          console.log('Deleting Category');
-          const { selectedId } = store.categoriesManager;
+    () => {
+      if (
+        location.pathname !== '/categories'
+      ) {
+        return null;
+      } else {
+        return (
+          <CategoryListActions
+            selectedCategoryId={store.categoriesManager.selectedId}
+            onCreateCategory={() => {
+              console.log('Routing to add category');
+              history.push('/categories/create');
+            }}
+            onEditCategory={() => {
+              console.log('Routing to edit category');
+              history.push(`/categories/edit/${store.categoriesManager.selectedId}`);
+            }}
+            onViewCategory={() => {
+              console.log('Also routing to edit category');
+              history.push(`/categories/edit/${store.categoriesManager.selectedId}`);
+            }}
+            onDeleteCategory={() => {
+              console.log('Deleting Category');
+              const { selectedId } = store.categoriesManager;
 
-          if (selectedId) {
-            store.categoriesManager.deleteCategory(selectedId);
-          }
-        }}
-      />
-    )
+              if (selectedId) {
+                store.categoriesManager.deleteCategory(selectedId);
+              }
+            }}
+          />
+        );
+      }
+    }
   );
 }
