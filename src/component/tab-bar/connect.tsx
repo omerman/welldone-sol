e0c2021cb3d5a3f7e66564df305af4fca9a2d86d
Omer Prizner <omer.prizner@gmail.com>
2019-11-11 09:08:55 +0200

    client - set the bottom tab bar behavior correctly

--- src/component/tab-bar/connect.tsx
+++ src/component/tab-bar/connect.tsx
@@ -1,13 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import { TabBar, ETabBarTab } from ".";
+import { useLocation, useHistory } from "react-router";
 
 export const ConnectTabBar: React.FC = () => {
-  const [selectedTab, setSelectedTab] = useState(ETabBarTab.categories);
+  let selectedTab: ETabBarTab;
 
+  const location = useLocation();
+  if (location.pathname.startsWith('/locations')) {
+    selectedTab = ETabBarTab.locations;
+  } else {
+    selectedTab = ETabBarTab.categories;
+  }
+
+  const history = useHistory();
   return (
     <TabBar
       selectedTab={selectedTab}
-      onSelectedTabChange={setSelectedTab}
+      onSelectedTabChange={(nextTab) => {
+        if (nextTab === selectedTab) {
+          return;
+        }
+
+        if (nextTab === ETabBarTab.categories) {
+          history.push('/categories');
+        } else {
+          history.push('/locations');
+        }
+      }}
     />
   )
 };
