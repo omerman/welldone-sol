dad19a3cb4e8d6146fe8baa3fb154dc7780ed27d
Omer Prizner <omer.prizner@gmail.com>
2019-11-15 12:34:11 +0200

    added default mui theme to app.

--- src/component/app/index.tsx
+++ src/component/app/index.tsx
@@ -6,8 +6,9 @@ import {
   Redirect,
 } from "react-router-dom";
 import { Toolbar } from '@material-ui/core';
-import { ConnectAppBar } from '../app-bar/connect';
 import { StoreProvider } from '../common/provider/store';
+import { ThemeProvider } from '../common/provider/theme';
+import { ConnectAppBar } from '../app-bar/connect';
 import { ConnectCategoryList } from '../category-list/connect';
 import { ConnectEditCategory } from '../category-editor/edit/connect';
 import { ConnectAddCategory } from '../category-editor/add/connect';
@@ -22,40 +23,42 @@ import { StyledTabBarWrap } from './styled/tab-bar-wrap';
 export const App: React.FC = () => {
   return (
     <StoreProvider>
-      <Router>
-        <StyledAppContentWrap>
-          <ConnectAppBar />
-          <Toolbar />
-          <StyledRouteWrap>
-            <Switch>
-              <Route path="/categories/edit/:categoryId">
-                <ConnectEditCategory />
-              </Route>
-              <Route path="/categories/create">
-                <ConnectAddCategory />
-              </Route>
-              <Route path="/categories">
-                <ConnectCategoryList />
-              </Route>
-              <Route path="/locations/edit/:locationId">
-                <ConnectEditLocation />
-              </Route>
-              <Route path="/locations/create">
-                <ConnectAddLocation />
-              </Route>
-              <Route path="/locations">
-                <ConnectLocationList />
-              </Route>
-              <Route path="/">
-                <Redirect to="/categories" />
-              </Route>
-            </Switch>
-          </StyledRouteWrap>
-          <StyledTabBarWrap>
-            <ConnectTabBar />
-          </StyledTabBarWrap>
-        </StyledAppContentWrap>
-      </Router>
+      <ThemeProvider>
+        <Router>
+          <StyledAppContentWrap>
+            <ConnectAppBar />
+            <Toolbar />
+            <StyledRouteWrap>
+              <Switch>
+                <Route path="/categories/edit/:categoryId">
+                  <ConnectEditCategory />
+                </Route>
+                <Route path="/categories/create">
+                  <ConnectAddCategory />
+                </Route>
+                <Route path="/categories">
+                  <ConnectCategoryList />
+                </Route>
+                <Route path="/locations/edit/:locationId">
+                  <ConnectEditLocation />
+                </Route>
+                <Route path="/locations/create">
+                  <ConnectAddLocation />
+                </Route>
+                <Route path="/locations">
+                  <ConnectLocationList />
+                </Route>
+                <Route path="/">
+                  <Redirect to="/categories" />
+                </Route>
+              </Switch>
+            </StyledRouteWrap>
+            <StyledTabBarWrap>
+              <ConnectTabBar />
+            </StyledTabBarWrap>
+          </StyledAppContentWrap>
+        </Router>
+      </ThemeProvider>
     </StoreProvider>
   );
 }
